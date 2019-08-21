69e46e48c2ffed05bbf9a83c1a32713a5bc008aa
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-08-21 11:09:25 +0900

    create server and client

---
+++ m3501/client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace m3501
+{
+    public partial class client : Form
+    {
+        public client()
+        {
+            InitializeComponent();
+        }
+    }
+}
