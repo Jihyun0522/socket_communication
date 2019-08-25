b1d11df6309b35b500f20fa8c7627ef939781c4b
jihyun0522 <s2017s01@e-mirim.hs.kr>
2019-08-25 23:45:08 +0900

    add HandleClient.cs

--- m3501/server.Designer.cs
+++ m3501/server.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.server_ip = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
             this.server_port = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.server_ip = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.create = new System.Windows.Forms.Button();
             this.stop = new System.Windows.Forms.Button();
             this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.change = new System.Windows.Forms.Button();
             this.server_name = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.change = new System.Windows.Forms.Button();
             this.server_messageBox = new System.Windows.Forms.TextBox();
             this.server_message = new System.Windows.Forms.TextBox();
             this.server_send = new System.Windows.Forms.Button();
@@ -60,23 +60,13 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "채팅방 정보";
             // 
-            // label1
+            // server_port
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.label1.Location = new System.Drawing.Point(53, 41);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(22, 19);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "IP";
-            // 
-            // server_ip
-            // 
-            this.server_ip.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.server_ip.Location = new System.Drawing.Point(104, 37);
-            this.server_ip.Name = "server_ip";
-            this.server_ip.Size = new System.Drawing.Size(288, 26);
-            this.server_ip.TabIndex = 1;
+            this.server_port.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.server_port.Location = new System.Drawing.Point(104, 80);
+            this.server_port.Name = "server_port";
+            this.server_port.Size = new System.Drawing.Size(288, 26);
+            this.server_port.TabIndex = 3;
             // 
             // label2
             // 
@@ -88,13 +78,23 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "PORT";
             // 
-            // server_port
+            // server_ip
             // 
-            this.server_port.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.server_port.Location = new System.Drawing.Point(104, 80);
-            this.server_port.Name = "server_port";
-            this.server_port.Size = new System.Drawing.Size(288, 26);
-            this.server_port.TabIndex = 3;
+            this.server_ip.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.server_ip.Location = new System.Drawing.Point(104, 37);
+            this.server_ip.Name = "server_ip";
+            this.server_ip.Size = new System.Drawing.Size(288, 26);
+            this.server_ip.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(53, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(22, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP";
             // 
             // create
             // 
@@ -129,6 +129,16 @@
             this.groupBox2.TabStop = false;
             this.groupBox2.Text = "내 정보 수정";
             // 
+            // change
+            // 
+            this.change.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.change.Location = new System.Drawing.Point(332, 34);
+            this.change.Name = "change";
+            this.change.Size = new System.Drawing.Size(74, 43);
+            this.change.TabIndex = 5;
+            this.change.Text = "변경";
+            this.change.UseVisualStyleBackColor = true;
+            // 
             // server_name
             // 
             this.server_name.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
@@ -147,16 +157,6 @@
             this.label4.TabIndex = 0;
             this.label4.Text = "이름";
             // 
-            // change
-            // 
-            this.change.Font = new System.Drawing.Font("나눔바른고딕", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
-            this.change.Location = new System.Drawing.Point(332, 34);
-            this.change.Name = "change";
-            this.change.Size = new System.Drawing.Size(74, 43);
-            this.change.TabIndex = 5;
-            this.change.Text = "변경";
-            this.change.UseVisualStyleBackColor = true;
-            // 
             // server_messageBox
             // 
             this.server_messageBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
@@ -165,6 +165,7 @@
             this.server_messageBox.Multiline = true;
             this.server_messageBox.Name = "server_messageBox";
             this.server_messageBox.ReadOnly = true;
+            this.server_messageBox.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.server_messageBox.Size = new System.Drawing.Size(423, 346);
             this.server_messageBox.TabIndex = 5;
             // 
--- m3501/server.cs
+++ m3501/server.cs
@@ -6,14 +6,65 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace m3501
 {
     public partial class server : Form
     {
+        TcpListener tcp = null; //서버
+        TcpClient clientSocket = null; //socket
+        static int counter = 0; //접속한 사용자 수
+        string date; //날짜
+        public Dictionary<TcpClient, string> clientList = new Dictionary<TcpClient, string>(); //각 client마다 리스트 추가
+
         public server()
         {
             InitializeComponent();
+            //Thread 생성
+            Thread t = new Thread(InitSocket);
+            t.IsBackground = true;
+            t.Start();
+        }
+
+        private void InitSocket()
+        {
+            tcp = new TcpListener(IPAddress.Any, 9999); // 서버 접속 IP, 포트
+            clientSocket = default(TcpClient); // 소켓 설정
+            tcp.Start(); // 서버 시작
+            server_messageBox.Text = ">> Server Started\n";
+            
+            while (true)
+            {
+                try
+                {
+                    counter++; // Client 수 증가
+                    clientSocket = tcp.AcceptTcpClient(); // client 소켓 접속 허용
+                    server_messageBox.AppendText(">> Accept connection From client\n");
+
+                    NetworkStream stream = clientSocket.GetStream();
+                    byte[] buffer = new byte[1024]; // 버퍼
+                    int bytes = stream.Read(buffer, 0, buffer.Length);
+                    string user_name = Encoding.Unicode.GetString(buffer, 0, bytes);
+                    user_name = user_name.Substring(0, user_name.IndexOf("$")); // client 사용자 명
+
+                    clientList.Add(clientSocket, user_name); // cleint 리스트에 추가
+
+                    SendMessageAll(user_name + " 님이 입장하셨습니다.", "", false); // 모든 client에게 메세지 전송
+
+                    handleClient h_client = new handleClient(); // 클라이언트 추가
+
+                    h_client.OnReceived += new handleClient.MessageDisplayHandler(OnReceived);
+                    h_client.OnDisconnected += new handleClient.DisconnectedHandler(h_client_OnDisconnected);
+                    h_client.startClient(clientSocket, clientList);
+                }
+                catch (SocketException se) { break; }
+                catch (Exception ex) { break; }
+            }
+            clientSocket.Close(); // client 소켓 닫기
+            tcp.Stop(); // 서버 종료
         }
     }
 }
